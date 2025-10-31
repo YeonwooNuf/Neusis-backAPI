5d8ad21a086199eb10fc5ad2e017e56a4b7b0901
장연우 <jangyw0927@naver.com>
2025-10-31 21:52:40 +0900

    first commit

---
+++ src/main/java/com/neusis/neusis_backend/NeusisBackendApplication.java
@@ -0,0 +1,13 @@
+package com.neusis.neusis_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeusisBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeusisBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neusis/neusis_backend/NeusisBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neusis.neusis_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeusisBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
