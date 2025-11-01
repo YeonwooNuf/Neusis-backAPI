80f46049d16380800b2b76161d9fd9f648c28520
장연우 <jangyw0927@naver.com>
2025-11-02 04:17:41 +0900

    // 전역 예외 처리 구조 틀

---
+++ src/main/java/com/neusis/backapi/exception/NotFoundException.java
@@ -0,0 +1,10 @@
+package com.neusis.backapi.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+// 전역 예외 처리 구조를 만들기 위한 기반
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) { super(message); }
+}
