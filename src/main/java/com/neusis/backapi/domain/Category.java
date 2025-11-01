e4e7dcea3e72f254ca4aefefe3950f15548eda65
장연우 <jangyw0927@naver.com>
2025-11-02 00:51:04 +0900

    // 기사 원문 카테고리 ENUM

---
+++ src/main/java/com/neusis/backapi/domain/Category.java
@@ -0,0 +1,12 @@
+package com.neusis.backapi.domain;
+
+public enum Category {
+    POLITICS,      // 정치
+    ECONOMY,       // 경제
+    SOCIETY,       // 사회
+    CULTURE,       // 문화
+    IT,            // IT/과학
+    SPORTS,        // 스포츠
+    ENTERTAINMENT, // 연예
+    ETC            // 기타
+}
