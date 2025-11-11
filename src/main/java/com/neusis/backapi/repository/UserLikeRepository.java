9fadba9111edd0df5b7b177d4bfa6d330a5e087d
장연우 <jangyw0927@naver.com>
2025-11-11 23:26:36 +0900

    // 사용자 좋아요(스크랩) DB 접근 계층
    좋아요는 반드시 사용자와 기사에 속해있어야 함

---
+++ src/main/java/com/neusis/backapi/repository/UserLikeRepository.java
@@ -0,0 +1,16 @@
+package com.neusis.backapi.repository;
+
+import com.neusis.backapi.domain.UserLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UserLikeRepository extends JpaRepository<UserLike, Long> {
+
+    boolean existsByUserUserIdAndArticleArticleId(Long userId, Long articleId);
+
+    long deleteByUserUserIdAndArticleArticleId(Long userId, Long articleId);
+
+    // 한 사용자가 목록 내 10개 기사에 대해 어떤 건 좋아요 눌렀고, 어떤 건 안 눌렀는지 표시
+    List<UserLike> findByUserUserIdAndArticleArticleIdIn(Long userId, List<Long> articleIds);
+}
