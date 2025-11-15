d9394242eca48a009a9e73bbf16c1999d05cd47b
장연우 <jangyw0927@naver.com>
2025-11-16 00:18:37 +0900

    // 로그인 api 구현
    요청 바디를 JSON으로 받고, DTO에서 검증 조건을 처리

--- src/main/java/com/neusis/backapi/controller/UserAccountController.java
+++ src/main/java/com/neusis/backapi/controller/UserAccountController.java
@@ -5,6 +5,7 @@ import com.neusis.backapi.service.UserAccountService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,11 +20,20 @@ public class UserAccountController {
     private final UserAccountService userAccountService;
 
     // 회원가입
+    @PostMapping("/auth/signup")
     public ResponseEntity<Long> signup(@Valid @RequestBody AuthDtos.SignupRequest req) {
         Long userId = userAccountService.signup(req);
 
         URI location = URI.create("/api/users/" + userId);
         // Location 헤더에 생성된 리소스의 URL을 넣어줌
         return ResponseEntity.created(location).body(userId);
+    }
+
+    // 로그인
+    public ResponseEntity<AuthDtos.LoginResponse> login(
+            @Valid @RequestBody AuthDtos.LoginRequest req   // 요청 바디를 JSON으로 받고, DTO에서 검증 조건을 처리
+    ) {
+        AuthDtos.LoginResponse resp = userAccountService.login(req);
+        return ResponseEntity.ok(resp);
     }
 }
