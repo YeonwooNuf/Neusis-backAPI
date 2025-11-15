4170748ac87279c70c91a80cc8e110adc77c8fbe
장연우 <jangyw0927@naver.com>
2025-11-16 01:08:36 +0900

    // 회원 탈퇴 api 구현
    getLoginUserOrThrow 로 세션 정보 가져옴과 동시에 세션 비어있을 경우 공통 예외 처리
    
    로그인 된 상태에서 탈퇴 요청 -> 바로 처리
    비로그인 상태에서 탈퇴 요청 -> 비밀번호 검증

--- src/main/java/com/neusis/backapi/controller/UserAccountController.java
+++ src/main/java/com/neusis/backapi/controller/UserAccountController.java
@@ -20,6 +20,19 @@ public class UserAccountController {
 
     private static final String LOGIN_USER_KEY = "LOGIN_USER";  // 세션 저장용 태그 이름
 
+    // 세션 정보 가져오기 및 공통 예외처리 메소드
+    private UserDto getLoginUserOrThrow(HttpSession session) {
+        // 세션 정보 가져오기
+        UserDto sessionUser = (UserDto) session.getAttribute(LOGIN_USER_KEY);
+        if (sessionUser == null) {
+            throw new org.springframework.web.server.ResponseStatusException(
+                    org.springframework.http.HttpStatus.UNAUTHORIZED,
+                    "로그인이 필요합니다."
+            );
+        }
+        return sessionUser;
+    }
+
     // 회원가입
     @PostMapping("/auth/signup")
     public ResponseEntity<Long> signup(@Valid @RequestBody AuthDtos.SignupRequest req) {
@@ -57,16 +70,28 @@ public class UserAccountController {
     @GetMapping("/users/me")
     public ResponseEntity<UserDto> getMyInfo(HttpSession session) {
 
-        // 세션 정보 가져오기
-        UserDto sessionUser = (UserDto) session.getAttribute(LOGIN_USER_KEY);
-
-        // 세션 비어있을 시
-        if(sessionUser == null) {
-            return ResponseEntity.status(401).build();
-        }
+        UserDto sessionUser = getLoginUserOrThrow(session);
 
         // 세션에 저장되어있는 userId 가져오기
         UserDto dto = userAccountService.getMyInfo(sessionUser.getUserId());
         return ResponseEntity.ok(dto);
+    }
+
+    @DeleteMapping("/users/me")
+    public ResponseEntity<Void> deleteUser(
+            HttpSession session,
+            @RequestBody(required = false) AuthDtos.DeleteRequest req
+    ) {
+        UserDto sessionUser = getLoginUserOrThrow(session);
+
+
+        // 로그인 된 상태면 → 그 안의 password 사용
+        // 비로그인 상태에서 탈퇴 요청 → 비밀번호 검증
+        String rawPassword = (req != null) ? req.getPassword() : null;
+        userAccountService.deleteMe(sessionUser.getUserId(), rawPassword);
+
+        // 세션 무효화
+        session.invalidate();
+        return ResponseEntity.noContent().build();
     }
 }
