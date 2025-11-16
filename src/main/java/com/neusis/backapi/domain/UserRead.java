3938f3d8a08e16ba459484cf1bc0e9680ec52de5
장연우 <jangyw0927@naver.com>
2025-11-16 20:47:33 +0900

    // readDate 컬럼 추가
    출석 일 수, 최근 조회 목록 구현 위함

--- src/main/java/com/neusis/backapi/domain/UserRead.java
+++ src/main/java/com/neusis/backapi/domain/UserRead.java
@@ -3,6 +3,8 @@ package com.neusis.backapi.domain;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.LocalDate;
+
 @Entity
 @Getter @Setter
 @NoArgsConstructor
@@ -17,6 +19,10 @@ public class UserRead {
     // 조회는 반드시 특정 사용자와 기사에 속해야 함
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     private User user;
+
+    // 조회한 날짜(출석 및 최근 읽은 카테고리 기록 위함)
+    @Column(nullable = false)
+    private LocalDate readDate;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     private Article article;
