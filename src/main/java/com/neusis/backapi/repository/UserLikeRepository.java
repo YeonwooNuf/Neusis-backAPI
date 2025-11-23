7615bac118f210b8e1901e5eaafe0ff8b88f4a1a
장연우 <jangyw0927@naver.com>
2025-11-24 00:09:28 +0900

    // 좋아요 누른 기사 목록 articleId 추출

--- src/main/java/com/neusis/backapi/repository/UserLikeRepository.java
+++ src/main/java/com/neusis/backapi/repository/UserLikeRepository.java
@@ -2,6 +2,8 @@ package com.neusis.backapi.repository;
 
 import com.neusis.backapi.domain.UserLike;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -12,6 +14,10 @@ public interface UserLikeRepository extends JpaRepository<UserLike, Long> {
     long deleteByUserUserId(Long userId);
 
     long deleteByUserUserIdAndArticleArticleId(Long userId, Long articleId);
+
+    // 좋아요 누른 기사 목록의 articleId 가져오기
+    @Query("SELECT ul.article.id FROM UserLike ul WHERE ul.user.id = :userId")
+    List<Long> findArticleIdsByUserId(@Param("userId") Long userId);
 
     // 한 사용자가 목록 내 10개 기사에 대해 어떤 건 좋아요 눌렀고, 어떤 건 안 눌렀는지 표시
     List<UserLike> findByUserUserIdAndArticleArticleIdIn(Long userId, List<Long> articleIds);
