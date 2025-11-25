ebb0bbc14f0e4ab12dc989d460beb3ccc6eca244
장연우 <jangyw0927@naver.com>
2025-11-25 17:08:24 +0900

    // 크롤링 구조에 맞게 ENUM 값 수정

--- src/main/java/com/neusis/backapi/domain/Category.java
+++ src/main/java/com/neusis/backapi/domain/Category.java
@@ -1,12 +1,10 @@
 package com.neusis.backapi.domain;
 
 public enum Category {
-    POLITICS,      // 정치
-    ECONOMY,       // 경제
-    SOCIETY,       // 사회
-    CULTURE,       // 문화
-    IT,            // IT/과학
-    SPORTS,        // 스포츠
-    ENTERTAINMENT, // 연예
-    ETC            // 기타
+    POLITICS,      // 정치(100)
+    ECONOMY,       // 경제(101)
+    SOCIETY,       // 사회(102)
+    CULTURE,       // 문화(103)
+    WORLD,         // 세계(104)
+    IT             // IT/과학(105)
 }
